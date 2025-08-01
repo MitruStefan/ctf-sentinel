81cfa7162ff65518dede5ceb84c65d2229353d8f
Stefan <82534183+MitruStefan@users.noreply.github.com>
2025-08-01 19:09:18 +0300

    StartDate and endDate fix for API (showing no upcoming)

--- api/api.js
+++ api/api.js
@@ -98,10 +98,9 @@ const getUpcomingEvents = async () => {
 	const events = await fetchAndCache(`events_${currentYear}`, `https://ctftime.org/api/v1/events/?limit=1000`);
 	if (!events) return null;
 	const startDate = new Date();
-	startDate.setDate(startDate.getDate() + ((1 - startDate.getDay()) % 7));
 	startDate.setHours(3, 0, 0, 0);
 	const endDate = new Date(startDate);
-	endDate.setDate(new Date().getDate() + 8);
+	endDate.setDate(startDate.getDate() + 7);
 	endDate.setHours(3, 0, 0, 0);
 
 	const upcomingEvents = events.filter(event => {
